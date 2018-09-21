d0c8654143d8a05c7a474d91ac9afc2dbed5aa2c
Antoniofis <Antoniofis@users.noreply.github.com>
2018-09-21 18:30:50 -0500

    First Page

---
+++ TelerikXamarinApp5/Portable/Models/TripLogEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelerikXamarinApp5.Portable.Models
+{
+    public class TripLogEntry
+    {
+        public string Title { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Date { get; set; }
+        public int Rating { get; set; }
+        public string Notes { get; set; }
+    }
+
+}
