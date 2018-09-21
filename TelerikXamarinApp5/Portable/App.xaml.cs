6db896560ea9dea49e7335696735b0150550db0b
Antoniofis <Antoniofis@users.noreply.github.com>
2018-09-21 18:52:28 -0500

    Second check in

--- TelerikXamarinApp5/Portable/App.xaml.cs
+++ TelerikXamarinApp5/Portable/App.xaml.cs
@@ -13,7 +13,7 @@ namespace TelerikXamarinApp5.Portable
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainPage());
 		}
 
 		protected override void OnStart ()
--- TelerikXamarinApp5/Portable/MainPage.xaml.cs
+++ TelerikXamarinApp5/Portable/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TelerikXamarinApp5.Portable.Models;
 using Xamarin.Forms;
 
 namespace TelerikXamarinApp5.Portable
@@ -12,6 +13,45 @@ namespace TelerikXamarinApp5.Portable
 		public MainPage()
 		{
 			InitializeComponent();
+
+
+            List<TripLogEntry> triplogs = new List<TripLogEntry>
+            {
+
+                new TripLogEntry
+                {
+                    Title ="Washington Monument",
+                    Notes ="Amazing!",
+                    Rating =3,
+                    Date =new DateTime(2017, 2, 5),
+                    Latitude=38.8895,
+                    Longitude=-77.0352         
+
+                },
+
+                new TripLogEntry
+                {
+                    Title ="Statue of Liberty",
+                    Notes ="Inspiring",
+                    Rating =4,
+                    Date =new DateTime(2017, 4, 13),
+                    Latitude=40.6892,
+                    Longitude=-74.0444
+                },
+
+                new TripLogEntry
+                {
+                    Title ="Golden Gate Bridge",
+                    Notes ="Foggy, but beautiful",
+                    Rating =5,
+                    Date =new DateTime(2017, 4, 26),
+                    Latitude=37.8268,
+                    Longitude=-122.4798
+
+                }
+            };
+
+            trips.ItemsSource = triplogs;
 		}
 	}
 }
